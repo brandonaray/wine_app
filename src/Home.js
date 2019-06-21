9b0d7b424abdbbc0224e8da020c4d9838c2c3c89
Lindsey Katz <katz.lindsey@gmail.com>
2019-06-20 20:27:01 -0500

    create wine.js, show list of wines on page

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,30 @@
 import React from "react";
 import Search from "./Search.js";
+import Axios from "axios";
+import data from "./data.json";
+import Wine from "./Wine.js";
 
 class Home extends React.Component {
   state = {
-    store: "new store"
+    store: "new store",
+    wines: []
   };
+
+  componentDidMount() {
+    this.setState({
+      wines: data.results
+    });
+    console.log(data.results);
+  }
 
   render() {
     return (
       <div className="Home">
         <p>{this.props.name}</p>
-        <p>{this.state.store}</p>
         <Search />
+        {this.state.wines.map(wine => {
+          return <Wine key={wine.wine_id} wine={wine} />;
+        })}
       </div>
     );
   }
